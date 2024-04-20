df09a45f4b34479abc489bde47c9e50ca711f6dd
vdave00 <162341004+vdave00@users.noreply.github.com>
2024-04-20 15:46:30 +0000

    data-type basics

---
+++ 01_basics/01_varibale.js
@@ -0,0 +1,21 @@
+const accountID = 12345;
+let accountEmail = "Vdave@Faang.com";
+var accountPassword ="vdave12345";
+accountCity = "Montreal"; // this is possiblity but do not use ..
+
+let accountstate; //it it upto you wan add semicolone or do not add ..
+
+// accountID = 2 // not allowed to change as it declare as const ...
+
+/* 
+-- perfer not to use var because of issue in scope and funcational scope ...
+alway use let to decalre variable ..
+*/
+
+// 
+accountEmail ="Vd@123.com";
+accountPassword="24242424";
+accountCity="Ahmedabad";
+
+console.table([accountID,accountEmail,accountPassword,accountPassword,accountstate]);
+console.log(accountID);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js  code as newer version ...
+
+// alert("Hello "); we are using node.js not browser so unable to use this ...
+
+
+console.log(3+3);
+console.log("Vijay");
+
+//number => 2 to power 53
+// string => ""
+//boolean => True/Fasle
+//null => standalone ..
+// undefine => value does not assign 
+
+//symbol => uniquesness .. tool for react .. 
+
+//object =>  userdefine data-type ... 
+
+console.log(typeof("Ram"));
+console.log(typeof null);  // object because as it standalone data type ...
+console.log(typeof undefined); // undefine .. 
