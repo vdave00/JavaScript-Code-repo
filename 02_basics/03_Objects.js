a9fc9852dd0ecf21e3f0f364d45027d49445af06
vdave00 <162341004+vdave00@users.noreply.github.com>
2024-04-23 14:43:12 +0000

    part1Objects

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -6,5 +6,18 @@
 
 const Jsuser = {
     name :"Vijay",
-    
+    isLoggedin :false,
+    lasLoggedin : ["monday","saturday"],
+    "Fname":"Vdave P"
 }
+
+// console.log(Jsuser.name);
+
+// console.log(Jsuser["name"]);
+
+// console.log(Jsuser["Fname"]);
+
+//updated file .. 
+
+
+// while using symbol use [] bracket and we can also freez the object than after do not able to update a value .. 
