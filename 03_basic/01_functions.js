d1e95410eaba219707f04186dd497b830dab7925
vdave00 <162341004+vdave00@users.noreply.github.com>
2024-04-23 17:21:32 +0000

    new commit

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,21 @@
+//  function addTwoNunmbers (n1,n2){
+//     return n1+n2
+//  }
+
+//  const result = addTwoNunmbers(5,9)
+
+//  console.log(result);
+
+//  function loginUserMessage(username){
+//     return `${username} just logged in!!`;
+//  }
+
+//  const login = loginUserMessage("Vijay");
+
+//  console.log(login);
+
+function calculatePrice(val1, val2 ,...num1){
+    return num1
+}
+
+console.log(calculatePrice(250,400,520));
